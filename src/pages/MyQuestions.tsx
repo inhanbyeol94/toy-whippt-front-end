cadc564d6e64e8eec75e0f5e45c7dc80a2eba9ba
inhanbyeol <inhanbyeol94@gmail.com>
2023-09-20 02:37:14 +0900

    Feat: MyQuestion Page
    '내 질문' 페이지의 메인 컴포넌트 입니다.
    사용되는 모든 컴포넌트가 선언되어있는 컴포넌트 입니다.

--- src/pages/MyQuestions.tsx
+++ src/pages/MyQuestions.tsx
@@ -1,3 +1,30 @@
+import { Layout, theme } from "antd";
+import { SidebarComponent } from "../components/whipGpt/Sidebar.component";
+import { IndexComponent } from "../components/myQuestion/Index.component";
+import React from "react";
+import { ModalComponent } from "../components/utils/Modal.component";
+import { MyQuestionModalComponent } from "../components/myQuestion/MyQuestion.modal.component";
+
+const { Content, Sider } = Layout;
 export const MyQuestions = () => {
-  return <>MyQuestions Page</>;
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <>
+      <Content style={{ padding: "0 50px", margin: "16px 0 0px 0" }}>
+        <Layout
+          style={{
+            padding: "24px 0",
+            background: colorBgContainer,
+            borderRadius: 10,
+            height: "84vh",
+          }}
+        >
+          <IndexComponent />
+        </Layout>
+      </Content>
+    </>
+  );
 };
