76fbac1fa14bba6ba1680312f4269cf5ddeeb4a9
inhanbyeol <inhanbyeol94@gmail.com>
2023-09-20 02:36:13 +0900

    Feat: MyQuestion Modal Component
    추 후 '내 질문' 영역에서 특정 질문의 자세히 보기를 클릭하였을 경우 모달을 구성할 수 있도록 경로만 추가한 상태입니다.
    추가작업 필요합니다.

---
+++ src/components/myQuestion/MyQuestion.modal.component.tsx
@@ -0,0 +1,10 @@
+import { ModalComponent } from "../utils/Modal.component";
+import React from "react";
+
+export const MyQuestionModalComponent = () => {
+  return (
+    <ModalComponent>
+      <span>TEST</span>
+    </ModalComponent>
+  );
+};
