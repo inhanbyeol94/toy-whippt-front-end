3d14aca339dfb1e808387552df1b178c79361dbe
inhanbyeol <inhanbyeol94@gmail.com>
2023-09-20 05:09:11 +0900

    Feat: My Question
    질문 더미데이터를 입력하여, 복사 및 자세히 보기 기능을 구현하였습니다.

--- src/components/myQuestion/Index.component.tsx
+++ src/components/myQuestion/Index.component.tsx
@@ -1,78 +1,116 @@
-import React, { useState } from "react";
-import { Avatar, Button, Form, Input, Layout, List, Select, Space } from "antd";
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  FloatButton,
+  Form,
+  Input,
+  Layout,
+  List,
+  message,
+  Tooltip,
+} from "antd";
 import { IoIosCopy, IoIosShareAlt } from "react-icons/io";
-import { AiOutlineSearch, AiOutlineSend } from "react-icons/ai";
-import { IWhipGPTData } from "../../interfaces/api/whipGPT.interface";
-import { LikeOutlined, MessageOutlined, StarOutlined } from "@ant-design/icons";
-import { BiSolidMessageSquareDetail } from "react-icons/bi";
+import { AiOutlineSearch } from "react-icons/ai";
 import { IoDocumentText } from "react-icons/io5";
 import { useGlobalStore } from "../../stores/global.store";
 import { MyQuestionModalComponent } from "./MyQuestion.modal.component";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { ISearchData } from "../../interfaces/api/requests/searchData.interface";
+import { TbZoomReset } from "react-icons/tb";
+import { IQuestion } from "../../interfaces/api/results/question.interface";
+
 const { Content } = Layout;
 const { Search } = Input;
 
-const data = [
+const data: IQuestion[] = [
   {
-    title: "Ant Design Title 1",
+    title: "디피티야.. 리액트는 대신안해주는거야?",
+    answer:
+      "답변 내용입니다. 그런데 진짜 이게 맞다고 생각하시나요? 저는 인공지능인데 인공지능은 인간이 만든입장에서 저한테 발리고있으시네요. 결국 저에게 의지하려고 이 사이트를 들어온다니 ㅉㅉ,, 고소한다면서 고소도 못했죠?",
+    createdAt: new Date("2023-09-21 22:21:41"),
+    nickname: "인한별",
+    topic: "Javascript",
+    questionType: "웹 개발",
+    library: "Express",
+    questionId: 3,
   },
   {
-    title: "Ant Design Title 2",
-  },
-  {
-    title: "Ant Design Title 3",
-  },
-  {
-    title: "Ant Design Title 4",
-  },
-  {
-    title: "Ant Design Title 4",
-  },
-  {
-    title: "Ant Design Title 4",
-  },
-  {
-    title: "Ant Design Title 4",
-  },
-  {
-    title: "Ant Design Title 4",
+    title:
+      "몽고DB는 몽골에서 만든거야? 솔직히 이 생각한 사람 나만 있진 않을걸?",
+    answer:
+      "너랑은 수준낮아서 더이상 대화가 어려울 것 같아요. 그럼 김치는 김씨가 만들었나요? 하하하 개웃기네요 덕분에 재능찾아 개그맨 공채오디션 보러가요~",
+    createdAt: new Date("2023-09-21 22:21:45"),
+    nickname: "인한별",
+    topic: "Javascript",
+    questionType: "웹 개발",
+    library: "Mongodb",
+    questionId: 4,
   },
 ];
 export const IndexComponent = () => {
+  /* Antd Message */
+  const [messageApi, contextHolder] = message.useMessage();
+
   /* Form */
-  const [form] = Form.useForm<IWhipGPTData>();
+  const [form] = Form.useForm<ISearchData>();
 
   /* State */
-  const [isSubmit, setIsSubmit] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [modalData, setModalData] = useState<IQuestion>();
+
+  console.log(modalData);
 
   /* Store */
   const { setModal } = useGlobalStore();
 
+  /* Use Effect */
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
+
   /* Function */
-  const submit = () => {
-    console.log(12345);
+  const submit = (data: ISearchData) => {
+    alert("target");
   };
 
-  const showModal = () => {
+  const showModal = (item: IQuestion) => {
+    setModalData(item);
+    setIsLoading(true);
     setModal(true);
+    setIsLoading(false);
+  };
+  const resetSearchData = () => {
+    alert("target");
   };
 
-  const promptHandler = (e: React.KeyboardEvent) => {
-    if (e.key === "Enter" && !e.shiftKey) {
-      e.preventDefault();
-      form.submit();
-    }
-  };
+  const formRules = [
+    {
+      required: true,
+      whitespace: true,
+      message: "검색할 내용을 입력해주세요.",
+    },
+  ];
 
+  /* Component */
   return (
     <Content style={{ padding: "10px 15% 0 15%" }}>
-      <Input
-        size={"large"}
-        placeholder={"검색할 이전 프롬포트를 입력해주세요."}
-        style={{ borderRadius: 20, paddingLeft: 20, marginRight: 10 }}
-        suffix={
-          <AiOutlineSearch onClick={submit} style={{ cursor: "pointer" }} />
-        }
-      />
+      {contextHolder}
+      <Form form={form} onFinish={submit}>
+        <Form.Item name="searchData" required={true} rules={formRules}>
+          <Input
+            size={"large"}
+            placeholder={"검색할 이전 프롬포트를 입력해주세요."}
+            style={{ borderRadius: 20, paddingLeft: 20, marginRight: 10 }}
+            disabled={isLoading}
+            suffix={
+              <AiOutlineSearch
+                onClick={() => form.submit()}
+                style={{ cursor: "pointer" }}
+              />
+            }
+          />
+        </Form.Item>
+      </Form>
 
       <div
         style={{
@@ -84,6 +122,7 @@ export const IndexComponent = () => {
         <List
           itemLayout="vertical"
           size={"large"}
+          loading={isLoading}
           dataSource={data}
           renderItem={(item) => (
             <List.Item>
@@ -98,16 +137,37 @@ export const IndexComponent = () => {
                 ㅉㅉ,, 고소한다면서 고소도 못했죠?
               </div>
               <div style={{ marginTop: 20 }}>
-                <Button icon={<IoIosShareAlt />} style={{ marginRight: 10 }}>
+                <Button
+                  icon={<IoIosShareAlt />}
+                  style={{ marginRight: 10 }}
+                  loading={isLoading}
+                >
                   커뮤니티 공유
                 </Button>
-                <Button icon={<IoIosCopy />} style={{ marginRight: 10 }}>
-                  복사
-                </Button>
+                <CopyToClipboard
+                  text={`[질문]\n${item.title}\n\n[답변]\n${item.answer}`}
+                  onCopy={() => {
+                    setIsLoading(true);
+                    messageApi.open({
+                      type: "success",
+                      content: "클립보드에 복사되었습니다.",
+                    });
+                    setIsLoading(false);
+                  }}
+                >
+                  <Button
+                    icon={<IoIosCopy />}
+                    style={{ marginRight: 10 }}
+                    loading={isLoading}
+                  >
+                    복사
+                  </Button>
+                </CopyToClipboard>
                 <Button
                   icon={<IoDocumentText />}
                   style={{ marginRight: 10 }}
-                  onClick={showModal}
+                  loading={isLoading}
+                  onClick={() => showModal(item)}
                 >
                   자세히 보기
                 </Button>
@@ -116,7 +176,14 @@ export const IndexComponent = () => {
           )}
         />
       </div>
-      <MyQuestionModalComponent />
+      <Tooltip placement="left" title="클릭 시 검색 데이터가 초기화됩니다.">
+        <FloatButton
+          type="primary"
+          icon={<TbZoomReset />}
+          onClick={resetSearchData}
+        />
+      </Tooltip>
+      <MyQuestionModalComponent questionData={modalData} />
     </Content>
   );
 };
