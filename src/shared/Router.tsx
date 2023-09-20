01192a02c0bbdaac8eabc1ca1a5197f43d9b139f
inhanbyeol <inhanbyeol94@gmail.com>
2023-09-20 21:17:38 +0900

    Feat: Login 페이지 구조

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Layout } from "antd";
+import GlobalStyles from "../GlobalStyles";
 import { AntLayout, AntFooter } from "../components/common/AntLayout";
 import { WhipGPT } from "../pages/WhipGPT";
 import { MyQuestions } from "../pages/MyQuestions";
 import { Study } from "../pages/Study";
 import { Community } from "../pages/Community";
-import GlobalStyles from "../GlobalStyles";
+import { Login } from "../pages/Login";
 const Router = () => {
   return (
     <BrowserRouter>
@@ -15,6 +16,7 @@ const Router = () => {
         <AntLayout />
         <Routes>
           <Route path="/" element={<WhipGPT />}></Route>
+            <Route path="/login" element={<Login />}></Route>
           <Route path="/my/questions" element={<MyQuestions />}></Route>
           <Route path="/community" element={<Community />}></Route>
           <Route path="/study" element={<Study />}></Route>
