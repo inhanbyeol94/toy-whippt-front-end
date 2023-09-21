7ab60b2d2d3146d265394f72f7e8063fe8954af9
inhanbyeol <inhanbyeol94@gmail.com>
2023-09-22 00:05:04 +0900

    Design: 로그인 컴포넌트 CSS

--- src/components/auth/signIn.style.ts
+++ src/components/auth/signIn.style.ts
@@ -4,17 +4,25 @@ import styled from "styled-components";
 const { Content } = Layout;
 
 export const S = {
-    Content: styled(Content)`
+  Content: styled(Content)`
     padding: 0 24px;
-    `,
-    Container: styled.div`
-      height: 79vh;
-      background-color: rgba(0, 0, 0, 0.03);
-      border-radius: 10px;
-    `,
-    LoginBox: styled.div`
-      width: 400px;
-      height: 200px;
-      background-color: var(--sub-color);
-    `
-}
+  `,
+  Container: styled.div`
+    height: 93.2vh;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+  LoginBox: styled.div`
+    width: 400px;
+    height: 200px;
+    border-radius: 10px;
+    background-color: var(--sub-color);
+    display: flex;
+    gap: 10px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  `,
+};
