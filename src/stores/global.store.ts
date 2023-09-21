0e441e11e2d8dcbe8764664380fed6101acd3e10
inhanbyeol <inhanbyeol94@gmail.com>
2023-09-22 00:00:40 +0900

    Feat: Global Store
    userInfo와 spin값을 관리할 수 있도록 추가하였습니다.

--- src/stores/global.store.ts
+++ src/stores/global.store.ts
@@ -1,7 +1,28 @@
 import { create } from "zustand";
 import { IGlobalStore } from "../interfaces/store/global.interface";
+import { IAuthInfo } from "../interfaces/api/results/authinfo.interface";
 
 export const useGlobalStore = create<IGlobalStore>((set) => ({
   modal: false,
   setModal: (modal) => set((state) => ({ modal })),
+  header: false,
+  setHeader: (header: boolean) => set((state) => ({ header })),
+  userInfo: {
+    id: "",
+    email: "",
+    name: "",
+    profileImgUrl: "",
+  },
+  setUserInfo: (userInfo: IAuthInfo) => set((state) => ({ userInfo })),
+  resetUserInfo: () =>
+    set((state) => ({
+      userInfo: {
+        id: "",
+        email: "",
+        name: "",
+        profileImgUrl: "",
+      },
+    })),
+  spin: true,
+  setSpin: (spin: boolean) => set((state) => ({ spin })),
 }));
