184c801cf88048732f2c03b014d1bbd74234bede
inhanbyeol <inhanbyeol94@gmail.com>
2023-09-24 05:58:44 +0900

    Test: 배포 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ export const queryClient = new QueryClient();
 
 // 배포 환경에서 console.log, console.warn, console.error 지우기
 if (process.env.NODE_ENV === "production") {
-    console.log = function no_console() {};
-    console.warn = function no_console() {};
-    console.error = function no_console() {};
+  console.log = function no_console() {};
+  console.warn = function no_console() {};
+  console.error = function no_console() {};
 }
-
+//
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
