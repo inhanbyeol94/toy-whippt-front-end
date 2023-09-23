165dd6961f1814b136d7243b922b2d614502ea5e
inhanbyeol <inhanbyeol94@gmail.com>
2023-09-24 03:55:53 +0900

    Design: 작성페이지 스타일 분리

--- src/pages/Write.tsx
+++ src/pages/Write.tsx
@@ -1,31 +1,31 @@
-import { Layout, theme } from "antd";
-import { SidebarComponent } from "../components/whipGpt/Sidebar.component";
-import { IndexComponent } from "../components/myQuestion/Index.component";
+import { Layout } from "antd";
 import React from "react";
-import { ModalComponent } from "../components/utils/Modal.component";
-import { MyQuestionModalComponent } from "../components/myQuestion/MyQuestion.modal.component";
 import { WriteComponent } from "../components/utils/Write.component";
+import styled from "styled-components";
 
-const { Content, Sider } = Layout;
+const { Content} = Layout;
 export const Write = () => {
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
 
   return (
     <>
-      <Content style={{ padding: "0 50px", margin: "16px 0 0px 0" }}>
-        <Layout
-          style={{
-            padding: "24px 0",
-            background: colorBgContainer,
-            borderRadius: 10,
-            height: "84vh",
-          }}
-        >
+      <S.Content>
+        <S.Layout>
           <WriteComponent />
-        </Layout>
-      </Content>
+        </S.Layout>
+      </S.Content>
     </>
   );
 };
+
+    const S = {
+        Content: styled(Content)`
+          padding: 0 50px;
+          margin: 16px 0 0 0;
+        `,
+        Layout: styled(Layout)`
+          height: 84vh;
+          padding: 24px 0;
+          border-radius: 10px;
+          background-color: var(--sub-color);
+        `,
+    }
